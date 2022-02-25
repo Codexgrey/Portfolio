c424d71c6f652631504e83dd65d269f711eeb581
Codexgrey <thecodexgrey@gmail.com>
2022-02-25 20:29:00 +0100

    wrote context.js

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import About from "./components/about/About.jsx";
 import Intro from "./components/intro/Intro.jsx";
 import DesignList from "./components/designList/DesignList.jsx";
 import Contact from "./components/contact/Contact.jsx";
+import Toggle from "./components/toggle/Toggle.jsx";
 
 // rendering components
 const App = () => {
   return (
       <div>
+        <Toggle />
         <Intro />
         <About />
         <DesignList />
