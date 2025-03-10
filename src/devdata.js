d37ae50f52bcdec5deabaa99d7e84c4a0e26697e
Collins Ovuakporaye <thecodexgrey@gmail.com>
2025-03-10 17:39:20 +0200

    house cleaning

--- src/devdata.js
+++ src/devdata.js
@@ -16,7 +16,7 @@ export const devworks = [
     {
         id: 1,
         title: "NASA Search App",
-        img: "https://www.dropbox.com/scl/fi/timpkfh7d8coj6kqtacze/screencapture-codexgrey-github-io-Social-Proof-Section-2024-04-03-15_34_10.png?rlkey=o74lgpx4tydkc2t77wsphpqfs&st=yyjag87j&dl=0",
+        img: "https://www.dropbox.com/scl/fi/timpkfh7d8coj6kqtacze/screencapture-codexgrey-github-io-Social-Proof-Section-2024-04-03-15_34_10.png?rlkey=o74lgpx4tydkc2t77wsphpqfs&st=yyjag87j&dl=1",
         link: "https://codexgrey.github.io/NASA-Search/",
         repo: "https://github.com/Codexgrey/NASA-Search",
     },
