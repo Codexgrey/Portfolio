867d35e8a526a8252eeb66bbd285222d95f248f5
Collins Ovuakporaye <thecodexgrey@gmail.com>
2026-03-25 12:16:24 +0200

    fix: image rendering issues

--- src/designdata.js
+++ src/designdata.js
@@ -10,42 +10,48 @@
 
     - DROPBOX change end of link from "dl=0" to "raw=1"
  */
+import DescomeMockUp from "./img/6Descome MockUp 2.jpg";
+import MusicAppMockUp from "./img/9Music App MockUp.jpg";
+import FoodAppMockUp from "./img/7Food App MockUp.jpg";
+import WebPractice from "./img/11Web Practice.png";
+import MusicLabelWebpage from "./img/10Music Label Webpage.png";
+import MobileDesign from "./img/8Mobile design.png";
 
 
 export const designs = [
     {
       id: 1,
-      img: "https://www.dropbox.com/s/hpx5jtlk2ivuvch/Descome%20MockUp%202.jpg?raw=1",
+      img: DescomeMockUp,
       link: "https://dribbble.com/codexgrey",
     },
 
     {
       id: 2,
-      img: "https://www.dropbox.com/scl/fi/oosf5fl9vx5mh0txcuox4/Music-App-MockUp-2.jpg?rlkey=qkjufm3yqa7mj74qoohy1m6g1&st=op63sipm&dl=1",
+      img: MusicAppMockUp,
       link: "https://dribbble.com/codexgrey",
     },
 
     {
       id: 3,
-      img: "https://www.dropbox.com/scl/fi/1v6weokn0e7hvqr97ph8i/Food-App-MockUp.jpg?rlkey=sc49pq7y392u5pahh966i38as&st=gghg0y7g&dl=1",
+      img: FoodAppMockUp,
       link: "https://dribbble.com/codexgrey",
     },
 
     {
       id: 4,
-      img: "https://www.dropbox.com/s/uv97exumga0o6nk/Web%20Practice.png?raw=1",
+      img: WebPractice,
       link: "https://dribbble.com/codexgrey",
     },
 
     {
       id: 5,
-      img: "https://www.dropbox.com/scl/fi/mwrjdtz2zw8z9hf854rpr/Music-Label-Webpage.png?rlkey=ia74yn0n7ovt741cyjukkkg9s&st=b8xoskxd&dl=1",
+      img: MusicLabelWebpage,
       link: "https://dribbble.com/codexgrey",
     },
 
     {
       id: 6,
-      img: "https://www.dropbox.com/s/qvlqb9lsgr1utgt/Mobile%20design.png?raw=1",
+      img: MobileDesign,
       link: "https://dribbble.com/codexgrey",
     },
 ];
--- src/devdata.js
+++ src/devdata.js
@@ -10,12 +10,19 @@
     - NOW* just -> remove "&st=76ttj6qk" and set dl=1
  */
 
+import NasaSearch from "./img/2NASA-Search.png";
+import Portfolio from "./img/0portfolio.png";
+import KAC233 from "./img/1KAC233.png";
+import SocialProofSection from "./img/4Social-Proof-Section.png";
+import ThePIGGame from "./img/3The-PIG-Game.png";
+import BudgetApp from "./img/5Budget-App.png";
+
 
 export const devworks = [
     {
         id: 1,
         title: "NASA API Search",
-        img: "https://www.dropbox.com/scl/fi/zw47ijzfztnnt14fcbbi0/screencapture-codexgrey-github-io-NASA-Search.png?rlkey=7c1mg93r2dsg57zjuzpk3tvrm&st=5ol6vubl&dl=1",
+        img: NasaSearch,
         link: "https://codexgrey.github.io/NASA-Search/",
         repo: "https://github.com/Codexgrey/NASA-Search",
     },
@@ -23,7 +30,7 @@ export const devworks = [
     {
         id: 2,
         title: "My portfolio",
-        img: "https://www.dropbox.com/scl/fi/qo8yxr7oyi0xmzy5h6xim/portfolio.png?rlkey=6tqa2e1350tqgboc0tcsf6dmh&st=0bmf4omj&dl=1",
+        img: Portfolio,
         link: "https://Codexgrey.github.io/Portfolio",
         repo: "https://github.com/Codexgrey/Portfolio",
     },
@@ -31,7 +38,7 @@ export const devworks = [
     {
         id: 3,
         title: "Events Website",
-        img: "https://www.dropbox.com/scl/fi/epevcc78vtcfu90c0qbmv/screencapture-codexgrey-github-io-KAC233-2024-04-03-15_28_32.png?rlkey=gt0xnaw6bt0xfr532npba8tej&st=t0k5k6re&dl=1",
+        img: KAC233,
         link: "https://codexgrey.github.io/KAC233/",
         repo: "https://github.com/Codexgrey/KAC233",
     },
@@ -39,7 +46,7 @@ export const devworks = [
     {
         id: 4,
         title: "Social Proof Section",
-        img: "https://www.dropbox.com/scl/fi/timpkfh7d8coj6kqtacze/screencapture-codexgrey-github-io-Social-Proof-Section-2024-04-03-15_34_10.png?rlkey=o74lgpx4tydkc2t77wsphpqfs&dl=1",
+        img: SocialProofSection,
         link: "https://codexgrey.github.io/Social-Proof-Section/",
         repo: "https://github.com/Codexgrey/Social-Proof-Section",
     },
@@ -47,7 +54,7 @@ export const devworks = [
     {
         id: 5,
         title: "Javascript - PIG GAME APP",
-        img: "https://www.dropbox.com/s/w7uaohrqbn7r0i6/screencapture-codexgrey-github-io-The-PIG-Game-2022-03-03-18_01_24.png?raw=1",
+        img: ThePIGGame,
         link: "https://codexgrey.github.io/The-PIG-Game/",
         repo: "https://github.com/Codexgrey/The-PIG-Game",
     },
@@ -55,7 +62,7 @@ export const devworks = [
     {
         id: 6,
         title: "BUDGET APP",
-        img: "https://www.dropbox.com/s/qort5i9z8guzbdg/screencapture-codexgrey-github-io-Budget-App-2022-03-03-17_57_36.png?raw=1",
+        img: BudgetApp,
         link: "https://codexgrey.github.io/Budget-App/",
         repo: "https://github.com/Codexgrey/Budget-App",
     },
